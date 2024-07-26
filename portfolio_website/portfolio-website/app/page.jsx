5c2df2097596348f256882fd56313b30e79310a6
Kanishk Bhatia <bhatia.ka@northeastern.edu>
2024-07-26 11:55:45 -0700

    Update page.jsx

--- portfolio_website/portfolio-website/app/page.jsx
+++ portfolio_website/portfolio-website/app/page.jsx
@@ -1,3 +1,35 @@
-export default function Home() {
-  return <main>homepage</main>
+import Social from "@/components/Social";
+import { Button } from "@/components/ui/button";
+import { FiDownload } from "react-icons/fi";
+
+const Home = () => {
+  return (
+    <section className="h-full">
+      <div className="container mx-auto h-full">
+        <div className="flex flex-col xl:flex-row items-center justify-between xl:pt-8 xl:pb-24">
+          <div className="text-center xl:text-left">
+            <span className="text-xl">Software Engineer</span>
+            <h1 className="h1 mb-6">
+              Hey I'm <br /> <span className="text-accent">Kanishk Bhatia</span>
+            </h1>
+            <p className="max-w-[500px] mb-9 text-white/80">
+              I design and develop experiences that make people's lives simple.
+            </p>
+            <div className="flex flex-col xl:flex-row items-center gap-8">
+              <Button variant="outline" size="lg" className="uppercase flex items-center gap-2">
+                <span>Download Resume</span>
+                <FiDownload className="text-xl"/>
+              </Button>
+              <div className="mb-8 xl:mb-0">
+                <Social containerStyles="flex gap-6" iconStyles="w-9 h-9 border border-accent rounded-full flex justify-center items-center text-accent text-base hover:bg-accent hover:text-primary hover:transition-all duration-500"/>
+              </div>
+            </div>
+          </div>
+          <div>photo</div>
+        </div>
+      </div>
+    </section>
+  )
 }
+
+export default Home
