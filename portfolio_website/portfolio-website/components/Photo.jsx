90e43515b560a64915bc642b689268518264c136
Kanishk Bhatia <bhatia.ka@northeastern.edu>
2024-07-26 18:58:15 -0700

    Create Photo.jsx

---
+++ portfolio_website/portfolio-website/components/Photo.jsx
@@ -0,0 +1,24 @@
+"use client";
+
+import { motion } from "framer-motion";
+import Image from "next/image";
+
+const Photo = () => {
+    return (
+        <div className="w-full h-full relative">
+            <motion.div initial={{opacity: 0}} animate={{opacity: 1, transition: {delay: 2, duration: 0.4, ease: 'easeIn'}}}>
+                <div className="w-[298px] h-[298px] xl:w-[498px] xl:h-[498] mix-blend-lighten">
+                    <Image
+                        src="/assets/Photo.jpg"
+                        priority
+                        quality
+                        fill
+                        alt=""
+                        className="object-contain"/>
+                </div>
+            </motion.div>
+        </div>
+    )
+}
+
+export default Photo
