35234189690e0548538f5c8465fb9fecc1b5feef
Kanishk Bhatia <bhatia.ka@northeastern.edu>
2024-07-26 18:58:05 -0700

    Update page.jsx

--- portfolio_website/portfolio-website/app/page.jsx
+++ portfolio_website/portfolio-website/app/page.jsx
@@ -1,3 +1,4 @@
+import Photo from "@/components/Photo";
 import Social from "@/components/Social";
 import { Button } from "@/components/ui/button";
 import { FiDownload } from "react-icons/fi";
@@ -21,11 +22,13 @@ const Home = () => {
                 <FiDownload className="text-xl"/>
               </Button>
               <div className="mb-8 xl:mb-0">
-                <Social containerStyles="flex gap-6" iconStyles="w-9 h-9 border border-accent rounded-full flex justify-center items-center text-accent text-base hover:bg-accent hover:text-primary hover:transition-all duration-500"/>
+                <Social containerStyles="flex gap-6" iconStyles="w-12 h-12 border border-accent rounded-full flex justify-center items-center text-accent text-base hover:bg-accent hover:text-primary hover:transition-all duration-500"/>
               </div>
             </div>
           </div>
-          <div>photo</div>
+          <div>
+            <Photo />
+          </div>
         </div>
       </div>
     </section>
