a413091b738c641eebb6997c71c42e49bf489973
Kanishk Bhatia <bhatia.ka@northeastern.edu>
2024-07-30 16:20:46 -0700

    Update Photo.jsx

--- portfolio_website/portfolio-website/components/Photo.jsx
+++ portfolio_website/portfolio-website/components/Photo.jsx
@@ -5,19 +5,18 @@ import Image from "next/image";
 
 const Photo = () => {
     return (
-        <div className="w-full h-full relative">
-            <motion.div initial={{opacity: 0}} animate={{opacity: 1, transition: {delay: 2, duration: 0.4, ease: 'easeIn'}}}>
-                <motion.div initial={{opacity: 0}} animate={{opacity: 1, transition: {delay: 2.4, duration: 0.4, ease: 'easeInOut'}}}
-                className="w-[298px] h-[298px] xl:w-[498px] xl:h-[498] mix-blend-lighten absolute">
-                    <Image
-                        src="/assets/photo.png"
-                        priority
-                        quality
-                        fill
-                        alt=""
-                        className="object-contain"/>
-                </motion.div>
-                <motion.svg className="w-[300px] xl:w-[506] h-[300px] xl:h-[506px]" fill="transparent" viewBox="0 0 506 506" xmlns="http://www.w3.org/2000/svg">
+        <div className="w-full h-full flex justify-center items-center relative xl:ml-10">
+            <motion.div 
+                initial={{opacity: 0}} 
+                animate={{opacity: 1, transition: {delay: 2, duration: 0.4, ease: 'easeIn'}}}
+                className="relative"
+            >
+                <motion.svg 
+                    className="absolute inset-0 w-[300px] h-[300px] xl:w-[506px] xl:h-[506px] m-auto z-20" 
+                    fill="transparent" 
+                    viewBox="0 0 506 506" 
+                    xmlns="http://www.w3.org/2000/svg"
+                >
                     <motion.circle 
                         cx="253" 
                         cy="253" 
@@ -27,18 +26,34 @@ const Photo = () => {
                         strokeLinecap="round" 
                         strokeLinejoin="round" 
                         initial={{strokeDasharray: "24 10 0 0"}} 
-                        animate= {{
+                        animate={{
                             strokeDasharray: ["15 120 25 25", "16 25 92 72", "4 250 22 22"], 
-                            rotate:[120, 360]}}
-                            transition={{
-                                duration: 20, 
-                                repeat: Infinity, 
-                                repeatType: "reverse",
-                            }}/>
+                            rotate:[120, 360]
+                        }}
+                        transition={{
+                            duration: 20, 
+                            repeat: Infinity, 
+                            repeatType: "reverse",
+                        }}
+                    />
                 </motion.svg>
+                <motion.div 
+                    initial={{opacity: 0}} 
+                    animate={{opacity: 1, transition: {delay: 2.4, duration: 0.4, ease: 'easeInOut'}}}
+                    className="relative z-10 w-[298px] h-[298px] xl:w-[498px] xl:h-[498px] overflow-hidden rounded-full"
+                >
+                    <Image
+                        src="/assets/photo.png"
+                        priority
+                        quality
+                        fill
+                        alt=""
+                        className="object-cover"
+                    />
+                </motion.div>
             </motion.div>
         </div>
     )
 }
 
-export default Photo
+export default Photo;
