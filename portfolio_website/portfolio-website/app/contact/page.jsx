e4a9916ecc4e4856085aab637725246d96a219a9
Kanishk Bhatia <bhatia.ka@northeastern.edu>
2024-08-02 01:48:47 -0700

    Update page.jsx

--- portfolio_website/portfolio-website/app/contact/page.jsx
+++ portfolio_website/portfolio-website/app/contact/page.jsx
@@ -1,7 +1,87 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Textarea } from "@/components/ui/textarea";
+import { Select, SelectContent, SelectItem, SelectLabel, SelectTrigger, SelectValue } from "@/components/ui/select";
+import { FaEnvelope, FaLinkedinIn, FaMapMarkedAlt } from "react-icons/fa";
+import { motion } from "framer-motion";
+
+const info = [
+    {
+        icon: <FaEnvelope />,
+        title: "Email",
+        description: "bhatiakanishk21@gmail.com",
+    },
+    {
+        icon: <FaLinkedinIn />,
+        title: "LinkedIn",
+        description: "linkedin.com/in/bhatiakanishk",
+        link: "https://www.linkedin.com/in/bhatiakanishk"
+    },
+    {
+        icon: <FaMapMarkedAlt />,
+        title: "Location",
+        description: "San Francisco, California",
+    },
+]
+
 const Contact = () => {
     return (
-        <div>Contact page</div>
+        <motion.div 
+            initial = {{opacity: 0}}
+            animate={{ opacity: 1, transition: {delay: 2.4, duration: 0.4, ease: "easeIn"},
+            }}
+            className="py-6">
+                <div className="container mx-auto">
+                    <div className="flex flex-col xl:flex-row gap-[30px]">
+                        <div className="xl:w-[54%] order-2 xl:order-none">
+                            <form className="flex flex-col gap-6 p-10 bg-[#27272c] rounded-xl">
+                                <h3 className="text-4xl text-accent">Let's work together</h3>
+                                <p className="text-white/60">
+                                    Drop a message and let's have a chat!
+                                </p>
+                                <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
+                                    <Input type="firstname" placeholder="First Name"/>
+                                    <Input type="lastname" placeholder="Last Name"/>
+                                    <Input type="email" placeholder="Email"/>
+                                </div>
+                                <Textarea className="h-[200px]" placeholder="Type you message here."/>
+                                <Button size="md" className="max-w-40">
+                                    Send message
+                                </Button>
+                            </form>
+                        </div>
+                        <div className="flex-1 flex items-center xl:justify-end order-1 xl:order-none mb-8 xl:mb-0">
+                            <ul className="flex flex-col gap-10">
+                                {info.map((item, index) => {
+                                    return (
+                                        <li key={index} className="flex items-center gap-6">
+                                            <div className="w-[52px] h-[52px] xl:w-[72px] xl:h-[72px] bg-[#27272c] text-accent rounded-md flex items-center justify-center">
+                                                <div className="text-[28px]">{item.icon}</div>
+                                            </div>
+                                            <div className="flex-1">
+                                                {item.link ? (
+                                                    <a href={item.link} target="_blank" rel="noopener noreferrer">
+                                                        <p className="text-white/60">{item.title}</p>
+                                                        <h3 className="text-xl">{item.description}</h3>
+                                                    </a>
+                                                ) : (
+                                                    <>
+                                                        <p className="text-white/60">{item.title}</p>
+                                                        <h3 className="text-xl">{item.description}</h3>
+                                                    </>
+                                                )}
+                                            </div>
+                                        </li>
+                                    )
+                                })}
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+        </motion.div>
     )
 }
 
-export default Contact
+export default Contact;
