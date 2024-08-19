063fa985736b91d81431846acf4870da33271abf
Kanishk Bhatia <bhatia.ka@northeastern.edu>
2024-08-19 10:31:53 -0700

    Update page.jsx

--- portfolio_website/portfolio-website/app/about/page.jsx
+++ portfolio_website/portfolio-website/app/about/page.jsx
@@ -36,7 +36,7 @@ const About = () => {
                         return <div key={index} className="flex-1 flex flex-col justify-center gap-6 group">
                             <div className="w-full flex justify-between items-center">
                                 <div className="text-5xl font-extrabold text-outline text-transparent group-hover:text-outline-hover transition-all duration-500">{about.num}</div>
-                                <Link href={about.href} className="w-[70px] h-[70px] rounded-full bg-white group-hover:bg-accent transition-all duration-500 flex justify-center items-center hover:-rotate-45">
+                                <Link href={about.href || "#"} className="w-[70px] h-[70px] rounded-full bg-white group-hover:bg-accent transition-all duration-500 flex justify-center items-center hover:-rotate-45">
                                     <BsArrowDownRight className="text-primary text-3xl"/>
                                 </Link>
                             </div>
